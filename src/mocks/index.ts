f96e3ce7e05d07c8ea04649f27ed606f25acf399
Mike Joyce <mike.joyce@shipt.com>
2023-04-13 11:17:51 -0400

    remove commented code

--- src/mocks/index.ts
+++ src/mocks/index.ts
@@ -5,18 +5,6 @@ async function initMocks() {
 
     server.listen();
   }
-  // Dont use SWC in client
-  //  else {
-  //   console.log("init mock service worker");
-  //   const { worker, rest } = await import("./browser");
-  //   // @ts-expect-error
-  //   window.msw = {
-  //     worker,
-  //     rest,
-  //   };
-
-  //   worker.start();
-  // }
 }
 
 initMocks();
